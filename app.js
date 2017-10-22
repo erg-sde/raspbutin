217f5be4f56cb0d5b1d0cd4f5b2511077abc1cff
Ed Gonzalez <ergonzalezpmp@gmail.com>
2017-10-22 03:37:31 -0400

    added list of phrases for dictionary training

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-matrix.service('face').start().then(function(data){
-  console.log(data);
-  matrix.led('blue').render();
-  setTimeout(function(){
-    matrix.led('black').render();
-  }, 2500);
-});
+matrix.led( 'yellow').render();
